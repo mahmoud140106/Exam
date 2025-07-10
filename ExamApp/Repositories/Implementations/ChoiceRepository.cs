0090bf68d765121364279e7b4cba20f96820592c
Ahmed Essam <ahmed.essam.aladl@gmail.com>
2025-07-10 21:04:22 +0300

    UnitOfWork

---
+++ ExamApp/Repositories/Implementations/ChoiceRepository.cs
@@ -0,0 +1,15 @@
+﻿using ExamApp.Models;
+using ExamApp.Repositories.Interface;
+
+namespace ExamApp.Repositories.Implementations
+{
+    public class ChoiceRepository:IChoiceRepository
+    {
+        public ChoiceRepository(ApplicationDbContext _db)
+        {
+            Db = _db;
+        }
+
+        public ApplicationDbContext Db { get; }
+    }
+}
---
+++ ExamApp/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using ExamApp.Models;
+using ExamApp.Repositories.Implementations;
+using ExamApp.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamApp.UnitOfWork
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        IExamRepository ExamRepo { get; }
+
+        IQuestionRepository QuestionRepo { get; }
+
+        IUserRepository UserRepo { get; }
+        IChoiceRepository ChoiceRepo { get; }
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+    }
+}
